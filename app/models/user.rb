302287bf5d85099cb440f885c625946d16d69cb8
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-30 21:10:08 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ActiveRecord::Base
   has_many :posts, through: :comments
   #telling ActiveRecord to look through the comments table to figure out this association. 
   #instances of User will respond to a method called .posts , and this returns a collection of posts that share a comment with the user. 
-  
 end
